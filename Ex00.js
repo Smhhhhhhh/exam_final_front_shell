60ba1a9e76d5238ede425cd8991e105db0f8b94b
Smhhhhhhh <113506448+Smhhhhhhh@users.noreply.github.com>
2022-11-24 14:41:31 +0100

    Create Ex00.js

---
+++ Ex00.js
@@ -0,0 +1,15 @@
+function pair(nombre){
+  if(nombre%2 == 0)
+              {
+                console.log("Even");
+                     return("Even");
+              }
+              else
+              {
+                  console.log("Odd");
+                     return("Odd");
+              }
+         }
+
+pair(14);
+
